2b9875ecd34a881293da34855227d1b48d22aebe
jeffreysnesbitt <50958843+jeffreysnesbitt@users.noreply.github.com>
2023-12-23 08:44:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
 let timerInterval;
-let seconds = 0;
-let selectedTimer = 2; // Default timer duration in seconds (changed for debugging)
-let isTimerRunning = false;
+let seconds = 2; // 30 seconds timer
 
-function startTimer(duration) {
-    selectedTimer = duration;
-    document.getElementById('timerOptions').style.display = 'none';
-    document.getElementById('startButton').textContent = `Start`;
-    document.getElementById('startButton').style.display = 'block';
-    document.getElementById('stopButton').style.display = 'block';
-    isTimerRunning = false;
-}
-
-function startTimerWithToggle() {
+function startTimer() {
     document.getElementById('startButton').style.display = 'none';
-    isTimerRunning = true;
-    seconds = selectedTimer; // Set initial seconds to the specified duration
-    updateTimer(); // Update the displayed timer immediately
+    document.getElementById('stopButton').style.display = 'block';
     timerInterval = setInterval(updateTimer, 1000);
 }
 
 function stopTimer() {
     clearInterval(timerInterval);
-    seconds = 0; // Reset the timer when stopped
-    updateTimer(); // Update the displayed timer immediately
-    document.getElementById('timerOptions').style.display = 'block';
-    document.getElementById('startButton').style.display = 'block';
-    document.getElementById('stopButton').style.display = 'none';
-    isTimerRunning = false;
+    resetTimer();
 }
 
 function updateTimer() {
@@ -40,15 +22,18 @@ function updateTimer() {
 
     if (seconds <= 0) {
         stopTimer();
-        if (isTimerRunning) {
-            document.getElementById('startButton').textContent = `Start`;
-            document.getElementById('startButton').style.display = 'block';
-            selectedTimer = (selectedTimer === 2) ? 4 : 2; // Reset to 4 seconds for 30 seconds or 2 seconds for 60 seconds
-        }
         return;
     }
 
     seconds--;
+}
+
+function resetTimer() {
+    clearInterval(timerInterval);
+    seconds = 30;
+    document.getElementById('timer').textContent = '00:00:30';
+    document.getElementById('startButton').style.display = 'block';
+    document.getElementById('stopButton').style.display = 'none';
 }
 
 
