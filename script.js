ea823285792055166ba2f8ef09e312da3f59fad6
jeffreysnesbitt <50958843+jeffreysnesbitt@users.noreply.github.com>
2023-12-23 09:20:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,17 @@ function updateTimer() {
         if (seconds <= 0) {
             seconds = 4;
             stopTimer(); // Stop the timer when it resets
+            changeButtonText(); // Change the button text when the timer resets
         }
 
         document.getElementById('timer').textContent = seconds;
     }
 }
+
+function changeButtonText() {
+    const startButton = document.getElementById('startButton');
+    if (startButton.textContent === '1-2') {
+        startButton.textContent = '1-3';
+    }
+}
+
