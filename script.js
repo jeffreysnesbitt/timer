9ae527ab4864d8385c981ebbb8364e75eca2bc17
jeffreysnesbitt <50958843+jeffreysnesbitt@users.noreply.github.com>
2023-12-23 09:39:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,30 @@ let buttonText = '1-2';
 
 function startTimer() {
     isRunning = true;
-@@ -28,8 +29,46 @@ function updateTimer() {
+    timer = setInterval(updateTimer, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timer);
+    isRunning = false;
+}
+
+function updateTimer() {
+    if (isRunning) {
+        seconds--;
+
+        if (seconds <= 0) {
+            seconds = 4;
+            stopTimer(); // Stop the timer when it resets
+            changeButtonText(); // Change the button text when the timer resets
+        }
+
+        document.getElementById('timer').textContent = seconds;
+    }
+}
 
 function changeButtonText() {
     const startButton = document.getElementById('startButton');
-    if (startButton.textContent === '1-2') {
-        startButton.textContent = '1-3';
 
     switch (buttonText) {
         case '1-2':
