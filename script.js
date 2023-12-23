22495da30474c8f6867059b3d025eb79ded6e7f7
jeffreysnesbitt <50958843+jeffreysnesbitt@users.noreply.github.com>
2023-12-23 08:00:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let timerInterval;
 let seconds = 0;
-let selectedTimer = 30; // Default timer duration in seconds
+let selectedTimer = 2; // Default timer duration in seconds
 let rangeCombinationIndex = 0;
 const rangeCombinations = ["1-2", "1-3", "1-4", "2-3", "2-4", "2-1", "3-4", "3-1", "3-2", "4-1", "4-2", "4-3"];
 
