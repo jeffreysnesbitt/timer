594156f1970926aa2cc31915f2d556c397690813
jeffreysnesbitt <50958843+jeffreysnesbitt@users.noreply.github.com>
2023-12-23 09:27:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let timer;
 let seconds = 4;
 let isRunning = false;
+let buttonText = '1-2';
 
 function startTimer() {
     isRunning = true;
@@ -28,8 +29,46 @@ function updateTimer() {
 
 function changeButtonText() {
     const startButton = document.getElementById('startButton');
-    if (startButton.textContent === '1-2') {
-        startButton.textContent = '1-3';
+
+    switch (buttonText) {
+        case '1-2':
+            buttonText = '1-3';
+            break;
+        case '1-3':
+            buttonText = '1-4';
+            break;
+        case '1-4':
+            buttonText = '2-3';
+            break;
+        case '2-3':
+            buttonText = '2-4';
+            break;
+        case '2-4':
+            buttonText = '2-1';
+            break;
+        case '2-1':
+            buttonText = '3-4';
+            break;
+        case '3-4':
+            buttonText = '3-1';
+            break;
+        case '3-1':
+            buttonText = '3-2';
+            break;
+        case '3-2':
+            buttonText = '4-1';
+            break;
+        case '4-1':
+            buttonText = '4-2';
+            break;
+        case '4-2':
+            buttonText = '4-3';
+            break;
+        case '4-3':
+            buttonText = '1-2';
+            break;
     }
+
+    startButton.textContent = buttonText;
 }
 
