29d95c9035963a71c049d050aa55fd4f5eacb29f
jeffreysnesbitt <50958843+jeffreysnesbitt@users.noreply.github.com>
2023-12-23 09:05:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let timer;
+let seconds = 4;
+
+function startTimer() {
+    timer = setInterval(updateTimer, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timer);
+}
+
+function updateTimer() {
+    seconds--;
+
+    if (seconds < 0) {
+        seconds = 4;
+    }
+
+    document.getElementById('timer').textContent = seconds;
+}
 
