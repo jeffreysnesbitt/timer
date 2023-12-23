8094ebc49866765b75aca1565275743eb629da20
jeffreysnesbitt <50958843+jeffreysnesbitt@users.noreply.github.com>
2023-12-23 07:10:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let timerInterval;
+let seconds = 0;
+
+function startTimer() {
+    timerInterval = setInterval(updateTimer, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
+    seconds = 0;
+    updateTimer();
+}
+
+function updateTimer() {
+    seconds++;
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+    document.getElementById('timer').textContent = formattedTime;
+}
 
