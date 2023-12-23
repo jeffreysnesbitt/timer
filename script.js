765e683db69d95639c88e18e6f83aeaa16c18232
jeffreysnesbitt <50958843+jeffreysnesbitt@users.noreply.github.com>
2023-12-23 09:15:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let timer;
 let seconds = 4;
+let isRunning = false;
 
 function startTimer() {
+    isRunning = true;
     timer = setInterval(updateTimer, 1000);
 }
 
 function stopTimer() {
     clearInterval(timer);
+    isRunning = false;
 }
 
 function updateTimer() {
-    seconds--;
+    if (isRunning) {
+        seconds--;
 
-    if (seconds <= 0) {
-        seconds = 4;
+        if (seconds <= 0) {
+            seconds = 4;
+            stopTimer(); // Stop the timer when it resets
+        }
+
+        document.getElementById('timer').textContent = seconds;
     }
-
-    document.getElementById('timer').textContent = seconds;
 }
-
